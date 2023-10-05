45a5485f3c9b3ce934d78b9b0c59e548bbad4620
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2023-10-05 20:51:42 +0500

    Add new features

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="wrapper d-flex flex-column align-items-center w-100">
         <h1 class="mb-5 h1 text-center">Todo Application</h1>
-        <form class="d-flex justify-content-center w-50 mb-3">
+        <form class="d-flex justify-content-center w-50 mb-3 ">
             <input v-model="searchQuery" class="query-input form-control mr-2" name="user_search_query" type="search" placeholder="Search your task here..." aria-label="Search your task here">
         </form>
         <form @submit.prevent="handleData" class="d-flex justify-content-center w-50 mb-5">
             <input v-model="formData.task" class="task-input form-control mr-2" name="user_task" type="text" placeholder="Add new task here..." aria-label="Add new task">
             <button class="btn btn-success px-4" type="submit">Add</button>
         </form>
-        <div class="w-50">
+        <div class="w-50 my-tasks">
             <h2 class="text-center mb-4">My tasks</h2>
             <ul class="list-group list-group-flush">
        
-            <div v-if="tasks.length == 0 || filteredTasks?.length == 0" class="w-100 border p-4 rounded text-center">
+            <div v-if="tasks.length == 0 || filteredTasks?.length == 0" class="w-100 border p-4 rounded text-center empty-box">
                 {{ filteredTasks?.length == 0 ? "Not found !" : "Task box is empty !" }}
             </div>
       
             <template v-for="task in filteredTasks == null ? tasks : filteredTasks" :key="task">
                 <li class="list-group-item bg-light" data-id="{{task.id}}">
-                    <form class="d-flex align-items-center justify-content-center">
+                    <form class="d-flex align-items-center justify-content-center check-form">
                         <input v-model="task.status" class="input-check" type="checkbox" :checked="task.status == true">
                     </form>
                       <p class="text text-muted m-0 p-0 task-title" :class="task.status && 'done'">{{task.task}}</p>
@@ -150,6 +150,23 @@
     text-decoration: none;
     color: green;
     content: "Done ✅";
+}
+
+@media only screen and (max-width: 800px) {
+
+    form {
+        width: 100% !important;
+    }
+
+    .check-form {
+        width: 5% !important;
+        display: inline-flex;
+        justify-content: flex-start !important;
+    }
+
+    .my-tasks {
+        width: 100% !important;
+    }
 }
 
 </style>
